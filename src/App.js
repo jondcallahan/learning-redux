73b23ed290f794ca1a496554beb8f431c8029e5e
Jon Callahan <jon@jons-mbp.attlocal.net>
2016-03-28 22:56:58 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux'
+
+// This is the reducer that manages the actions
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+
+    case 'DECREMENT':
+      return state -1;
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter)
+
+const App = ({value, onIncrement, onDecrement}) => {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1 style={{fontFamily: 'Open Sans', fontWeight: 300, color: '#333'}}>
+          {value}
+        </h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+      </div>
+    );
+  }
+
+render(
+  <App
+  value={store.getState()}
+  onIncrement={() =>
+    store.dispatch({
+      type: 'INCREMENT'
+    })
+  }
+  onDecrement={() =>
+    store.dispatch({
+      type: 'DECREMENT'
+    })
+  }
+  />,
+ document.getElementById('app'));
+
+store.subscribe(render)
