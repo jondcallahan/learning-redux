0aeac91ae436f951027d2546f19ac325f64820da
Jon Callahan <jon@Jons-MacBook-Pro.local>
2016-04-09 13:58:06 -0700

    changing objects with pure functions and object.assign

--- src/App.js
+++ src/App.js
@@ -1,59 +1,31 @@
 import expect from 'expect'
 import deepFreeze from 'deep-freeze'
 
-const addCounter = (list) => {
-  return [...list, 0]
+const toggleTodo = (todo) => {
+  return Object.assign({}, todo, {
+    completed: !todo.completed
+  })
 }
 
-const testAddCounter = () => {
-  const listBefore = []
-  const listAfter = [0]
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux!',
+    completed: false,
+  }
 
-  deepFreeze(listBefore)
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux!',
+    completed: true,
+  }
+
+  deepFreeze(todoBefore)
 
   expect(
-    addCounter(listBefore)
-  ).toEqual(listAfter)
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter)
 }
 
-const removeItem = (list, index) => {
-  return [
-    ...list.slice(0, index),
-    ...list.slice(index + 1)
-  ]
-}
-
-const testRemoveItem = () => {
-  const listBefore = [0, 10, 20]
-  const listAfter = [0, 20]
-
-  deepFreeze(listBefore)
-
-  expect(
-    removeItem(listBefore, 1)
-  ).toEqual(listAfter)
-}
-
-const incrementItem = (list, index) => {
-  return [
-    ...list.slice(0, index),
-    list[index] + 1,
-    ...list.slice(index + 1)
-  ]
-}
-
-const testIncrementItem = () => {
-  const listBefore = [0, 10, 20]
-  const listAfter= [0, 11, 20]
-
-  deepFreeze(listBefore)
-
-  expect(
-    incrementItem(listBefore, 1)
-  ).toEqual(listAfter)
-}
-
-testAddCounter()
-testRemoveItem()
-testIncrementItem()
+testToggleTodo()
 console.log('it worked!');
